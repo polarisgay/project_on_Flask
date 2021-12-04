29ee2411e5b30ac6ec2580f2ae48da669491a6d8
polarisgay <95522265+polarisgay@users.noreply.github.com>
2021-12-04 12:11:28 +0400

    Add files via upload

---
+++ site/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+menu = ['Установка', "Первое приложение", "Обратная свзяь"]
+
+@app.route("/")
+def index():
+    print(url_for('index'))
+    return render_template("index.html", menu = menu)
+
+@app.route("/about")
+def about():
+    print(url_for('about'))
+    return render_template("about.html", title = "О сайте", menu = menu)
+
+@app.route('/profile/<username>')
+def profile(username, path):
+    return f'Пользователь: {username}, {path}'
+
+# with app.test_request_context():
+#    print(url_for('index'))
+
+if __name__ =="__main__":
+    app.run(debug=True)
