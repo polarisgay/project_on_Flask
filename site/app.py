957a7b214257a3ebc6872b9d8542cb3fbab92812
polarisgay <95522265+polarisgay@users.noreply.github.com>
2021-12-06 13:53:46 +0400

    Add files via upload

--- site/app.py
+++ site/app.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template, url_for, request, flash
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '46280'
 
-menu = [{'name': 'Установка', 'url': 'install-flask'},
-        {'name': 'Первое приложение', 'url': 'first-app'},
-        {'name': 'Обратная свять', 'url': 'contact'}]
+menu = [{'name': 'Главная', 'url': '/'},
+        {'name': 'Каталог', 'url': 'first-app'},
+        {'name': 'Обратная свять', 'url': 'contact'},
+        {'name': 'Профиль', 'url': 'profile/''username'}]
 
 @app.route("/")
 def index():
@@ -20,11 +21,12 @@ def about():
 @app.route('/profile/<username>')
 def profile(username):
     return f'Пользователь: {username}'
+    return render_template(menu = menu)
 
 @app.route('/contact', methods = ['POST', 'GET'])
 def contact():
     if request.method == "POST":
-        if len('username') > 2:
+        if len(request.form['username']) > 2:
             flash('ЧЕЛ ХАРОШ')
         else:
             flash('КРИНЖАНУЛ')
